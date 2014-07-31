b7c4dd7e93e2941147f5018347ddbd753e7fdd76
jAbeJDev <jordan_68_2006@hotmail.com>
2014-07-31 18:48:43 -0500

    Update fgfg.java

--- GG/src/gg/fgfg.java
+++ GG/src/gg/fgfg.java
@@ -4,6 +4,7 @@ public class fgfg {
 	public static void main(String[] args) {
 		System.out.println("sos una mierda");
 		System.out.println("tu tambien");
+		System.out.println("tu tambien");
 		
 	}
 
