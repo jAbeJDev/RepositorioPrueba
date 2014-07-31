f8abfdc3d94f831d4307a9de0c6cc0625a3923f1
jAbeJDev <jordan_68_2006@hotmail.com>
2014-07-31 17:17:28 -0500

    ok

--- Pruebita/src/demo/com/PruebaManger.java
+++ Pruebita/src/demo/com/PruebaManger.java
@@ -3,6 +3,7 @@ package demo.com;
 public class PruebaManger {
 	public static void main(String[] args) {
 		System.out.println("hola mundo, este es mi repositorio");
+		System.out.println("hola mundo");
 	}
 
 }
